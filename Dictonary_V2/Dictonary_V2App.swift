528f25e92d567574b638b3e12f1a66022fea5a68
Eric Sousa <eu2001@gmail.com>
2023-03-06 14:51:45 -0500

    Initial Commit

---
+++ Dictonary_V2/Dictonary_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Dictonary_V2App.swift
+//  Dictonary_V2
+//
+//  Created by Eric Sousa on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Dictonary_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
